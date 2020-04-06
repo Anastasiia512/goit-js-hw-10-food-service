068c786746c5074e580c2723bf4e2b88cc2854fd
Anastasiia <nastyona221985@gmail.com>
2020-04-06 15:27:13 +0300

    hw-10

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { Theme } from './themes';
+import './styles.css';
+
+const input = document.querySelector('input.js-switch-input');
+
+if (localStorage.getItem('color-theme') !== null) {
+  const currentTheme = localStorage.getItem('color-theme');
+  document.body.classList.add(currentTheme);
+  if (document.body.classList.contains(Theme.DARK)) {
+    input.checked = true;
+  }
+}
+
+const changeTheme = function (e) {
+  if (e.target.checked) {
+    document.body.classList.add(Theme.DARK);
+    localStorage.setItem('color-theme', Theme.DARK);
+    document.body.classList.remove(Theme.LIGHT);
+    return;
+  }
+  document.body.classList.add(Theme.LIGHT);
+  localStorage.setItem('color-theme', Theme.LIGHT);
+  document.body.classList.remove(Theme.DARK);
+};
+
+input.addEventListener('change', changeTheme);
